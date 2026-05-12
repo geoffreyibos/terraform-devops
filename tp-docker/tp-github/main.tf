e902dabc3376ae28bcdaa937c7a1bf0625bb8e4a
Geoffrey <geoffrey.ibos@efrei.net>
2026-05-12 16:20:36 +0200

    create managed repository with secret and branch protection

---
+++ tp-docker/tp-github/main.tf
@@ -0,0 +1,22 @@
+resource "github_repository" "app" {
+ name = "${var.project_name}-demo"
+ description = "Dépôt géré par Terraform - DevOps 4A"
+ visibility = "public"
+ has_issues = true
+ auto_init = true # Initialise avec un README
+}
+
+resource "github_branch_protection" "main" {
+ repository_id = github_repository.app.node_id
+ pattern = "main"
+ required_pull_request_reviews {
+ required_approving_review_count = 1
+ dismiss_stale_reviews = true
+ }
+}
+
+resource "github_actions_secret" "db_url" {
+ repository = github_repository.app.name
+ secret_name = "DATABASE_URL"
+ plaintext_value = var.db_url
+}
