747e5fed641444251d0d649a0447e0ee4ec27481
Geoffrey <geoffrey.ibos@efrei.net>
2026-05-12 15:22:24 +0200

    add redis container

--- tp-docker/main.tf
+++ tp-docker/main.tf
@@ -1,17 +1,33 @@
 resource "docker_image" "nginx" {
- name = "nginx:alpine"
+  name = "nginx:alpine"
 }
+
 resource "docker_network" "app" {
- name = "app-network"
+  name = "app-network"
 }
+
 resource "docker_container" "web" {
- name = "${var.project_name}-web"
- image = docker_image.nginx.image_id
- ports {
- internal = 80
- external = var.host_port
- }
- networks_advanced {
- name = docker_network.app.name
- }
+  name  = "${var.project_name}-web"
+  image = docker_image.nginx.image_id
+  ports {
+    internal = 80
+    external = var.host_port
+  }
+  networks_advanced {
+    name = docker_network.app.name
+  }
 }
+
+resource "docker_image" "redis" {
+  name = "redis:alpine"
+}
+
+resource "docker_container" "redis" {
+  name  = "${var.project_name}-redis"
+  image = docker_image.redis.image_id
+
+  networks_advanced {
+    name = docker_network.app.name
+  }
+}
+
