e4e23fe18896856bc13b72bf3ac3317e3ffdad36
Geoffrey <geoffrey.ibos@efrei.net>
2026-05-13 19:06:13 +0200

    add web security group

--- tp-terraform-aws/main.tf
+++ tp-terraform-aws/main.tf
@@ -43,3 +43,45 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_security_group" "web" {
+  name        = "${var.project_name}-web-sg"
+  description = "Security Group pour le serveur web"
+  vpc_id      = aws_vpc.main.id
+  # SSH - uniquement depuis votre IP
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+    description = "SSH depuis ma machine"
+  }
+
+  # HTTP public
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP public"
+  }
+
+  # HTTPS public
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTPS public"
+  }
+
+  # Tout le trafic sortant autorisé
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Tout le trafic sortant"
+  }
+  tags = { Name = "${var.project_name}-web-sg" }
+}
